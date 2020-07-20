31dc70bf87187706c6b3cb4f3cb325bf2238bf2e
Lorenzo Gatti <lorenzo.gatti@alpiq.com>
2020-07-20 17:56:30 +0200

    Fix bug lambda package

--- s3.tf
+++ s3.tf
@@ -5,14 +5,23 @@ resource "aws_s3_bucket" "this" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm     = "AES256"
+        sse_algorithm = "AES256"
       }
     }
   }
   force_destroy = true
-  tags = var.tags
+  tags          = var.tags
 }
 
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+}
 # ------------------------------------------------------------------------------
 # Add notification to lambda
 # ------------------------------------------------------------------------------
